ac2bc8b74ee6a6cd18c486892f081f080a504afe
aryanpx <apitliya@bvurinnovations.com>
2024-04-24 15:43:36 +0530

    animation added

--- src/app/services/scroll-animation.service.ts
+++ src/app/services/scroll-animation.service.ts
@@ -10,37 +10,39 @@ export class ScrollAnimationService {
   initScrollAnimation(): void {
     $(window).scroll(() => {
       var scrollTop = $(window)?.scrollTop();
-      console.log('🚀 ~ ScrollAnimationService ~ $ ~ scrollTop:', scrollTop);
-      var phoneOffsetTop = $('.phone')?.offset()?.top;
-      console.log('🚀 ~ ScrollAnimationService ~ $ ~ phoneOffsetTop:', phoneOffsetTop);
+      var phoneOffsetTop = $('.frame1')?.offset()?.top;
       var windowHeight = $(window)?.height();
       var phoneWidth = $('.phone')?.width();
       // Check if phone image is in viewport
       if (phoneOffsetTop && scrollTop && windowHeight && phoneWidth) {
         let rotationAngle = Math.max(0, Math.min((scrollTop - phoneOffsetTop) * 0.1, 90)) * -1;
         console.log('🚀 ~ ScrollAnimationService ~ $ ~ rotationAngle:', rotationAngle);
-        if (0 > rotationAngle && rotationAngle > -90) {
+        if (rotationAngle > -90) {
           let opacity = 1 - Math.max(0, Math.min((scrollTop - phoneOffsetTop) / windowHeight, 1)) * 6;
-          console.log('🚀 ~ ScrollAnimationService ~ $ ~ opacity:', opacity);
+          // console.log('🚀 ~ ScrollAnimationService ~ $ ~ opacity:', opacity);
           let translateX = (rotationAngle / 90) * (window.innerWidth / 2 - phoneWidth / 2) * -1;
           console.log('🚀 ~ ScrollAnimationService ~ $ ~ translateX:', translateX);
           let translateY = Math.max(0, scrollTop - phoneOffsetTop);
-          console.log('🚀 ~ ScrollAnimationService ~ $ ~ translateY:', translateY);
-          if (phoneOffsetTop < scrollTop) {
-            // Apply rotation and fading animations
-            $('.phone').css('transform', `translate(${translateX}px, ${translateY}px) rotate(${rotationAngle}deg)`);
-            $('.paragraph').css('opacity', opacity);
-            $('.phone').removeClass('half-size');
-          } else {
-            if (scrollTop < this.lastScrollTop && scrollTop < phoneOffsetTop) {
-              $('.phone').css('transform', `translate(${translateX}px, ${translateY}px) rotate(${rotationAngle}deg)`);
-              $('.paragraph').css('opacity', opacity);
-              $('.phone').addClass('half-size');
+          // console.log('🚀 ~ ScrollAnimationService ~ $ ~ translateY:', translateY);
+          $('.phone').css('transform', `translate(${translateX}px, ${translateY}px) rotate(${rotationAngle}deg)`);
+          $('.paragraph').css('opacity', opacity);
+        } else {
+          if (scrollTop > this.lastScrollTop) {
+            if (phoneOffsetTop < scrollTop) {
+              console.log('scroll up');
+              // $('.paragraph').addClass('fade-out');
+              // $('.phone').removeClass('half-size');
+            } else {
+              if (scrollTop < phoneOffsetTop) {
+                console.log('scroll down');
+                // $('.paragraph').removeClass('fade-out');
+                // $('.phone').addClass('half-size');
+              }
             }
           }
         }
-        // Update last scroll top position
         this.lastScrollTop = scrollTop;
+        // console.log('🚀 ~ ScrollAnimationService ~ $ ~ this.lastScrollTop:', this.lastScrollTop);
       }
     });
   }
