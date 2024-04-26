ecb8f8e63f8425f4c0e3b1d9aa4cfe516fe59eca
aryanpx <apitliya@bvurinnovations.com>
2024-04-26 13:53:26 +0530

    blogs routing fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
 
 export const routes: Routes = [
-  { path: 'Home', title: 'Furrcrew-V2', component: AppComponent },
+  { path: '', title: 'Furrcrew-V2', component: BodyComponent },
   { path: 'Blogs', title: 'Blogs', component: BlogsComponent },
 ];
