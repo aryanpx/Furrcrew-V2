69de10373186d6a85d6d6f3a7b80a6f27532888c
aryanpx <apitliya@bvurinnovations.com>
2024-04-29 22:31:48 +0530

    routes and active tab fix

--- src/app/components/blogs-list/blogs-list.component.ts
+++ src/app/components/blogs-list/blogs-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blogs-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blogs-list.component.html',
   styleUrl: './blogs-list.component.css',
 })
