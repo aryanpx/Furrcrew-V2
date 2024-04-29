f32778395dc043a7cfbfc7371cd0c7d8f11ca47f
aryanpx <apitliya@bvurinnovations.com>
2024-04-29 22:29:34 +0530

    video screen size optimized

--- src/app/components/pet-video-component/pet-video-component.component.ts
+++ src/app/components/pet-video-component/pet-video-component.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-pet-video-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-video-component.component.html',
-  styleUrl: './pet-video-component.component.css'
+  styleUrl: './pet-video-component.component.css',
 })
 export class PetVideoComponentComponent {
-
+  isMobile: boolean = false;
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    if (typeof window !== 'undefined') {
+      this.isMobile = window.innerWidth < 769;
+    }
+  }
+  mobileVideoPath = 'assets/videos/pet_video_portrait.mp4';
+  desktopVideoPath = 'assets/videos/pet_video.mp4';
 }
