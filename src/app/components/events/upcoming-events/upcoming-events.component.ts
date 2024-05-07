09d7077397fb425281abbb0bc8ce0d45236a89e8
aryanpx <apitliya@bvurinnovations.com>
2024-05-07 14:51:37 +0530

    card flip animation done

--- src/app/components/events/upcoming-events/upcoming-events.component.ts
+++ src/app/components/events/upcoming-events/upcoming-events.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventsService } from '../../../services/events.service';
 import { FooterComponent } from '../../footer/footer.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-upcoming-events',
@@ -12,13 +13,37 @@ import { FooterComponent } from '../../footer/footer.component';
 })
 export class UpcomingEventsComponent implements OnInit {
   event: any;
+  isMobileView: boolean = false;
 
-  constructor(private route: ActivatedRoute, public eventsService: EventsService) {}
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    // Listen for window resize events
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    // Check screen width and set isMobileView accordingly
+    this.isMobileView = window.innerWidth <= 768; // Adjust the breakpoint as needed
+  }
+
+  constructor(private route: ActivatedRoute, public eventsService: EventsService, @Inject(PLATFORM_ID) private platformId: Object) {
+    // Check initial screen size on component initialization
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize();
+    }
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       const eventId = +params['eventId']; // Convert to number
       this.event = this.eventsService.events.find((event) => event.id === eventId);
     });
+  }
+  isFlipped: boolean = false;
+
+  flipCard() {
+    if (!this.isMobileView) {
+      this.isFlipped = !this.isFlipped;
+    }
   }
 }
