676ccc37918daca3c84b71b0fdb4a40a43965dbd
aryanpx <apitliya@bvurinnovations.com>
2024-05-07 08:57:56 +0530

    animation and mobile view done

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { Router, RouterLink } from '@angular/router';
 import { EventsService } from '../../services/events.service';
+import { CommonModule, DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink, CommonModule],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css',
 })
 export class EventsComponent {
-  constructor(private router: Router, public eventsService: EventsService) {}
+  imageWidth: string = '66.666667%';
+  isMobile: boolean = false;
+  constructor(private router: Router, public eventsService: EventsService, @Inject(DOCUMENT) private document: Document) {
+    if (typeof window !== 'undefined') {
+      this.isMobile = window.innerWidth < 769;
+    }
+  }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition < window.innerHeight) {
+      const widthIncrease = (scrollPosition / window.innerHeight) * 33.333333; // Calculate the percentage increase based on scroll
+      this.imageWidth = `${66.666667 + widthIncrease}%`;
+    } else {
+      this.imageWidth = '100%'; // Once the scroll is past the screen height, ensure image is full width
+    }
+  }
 
   goToEventDetails(eventId: number) {
     this.router.navigate(['/events/upcoming-events'], { queryParams: { eventId: eventId } });
