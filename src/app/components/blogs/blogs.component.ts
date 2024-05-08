fa1671d068122ca09e95b8f97f727139fb85dbcf
aryanpx <apitliya@bvurinnovations.com>
2024-05-08 10:54:40 +0530

    search and mobile view fix

--- src/app/components/blogs/blogs.component.ts
+++ src/app/components/blogs/blogs.component.ts
@@ -4,16 +4,17 @@ import { BlogsService } from '../../services/blogs.service';
 import { FooterComponent } from '../footer/footer.component';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../services/api.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blogs',
   standalone: true,
-  imports: [FooterComponent, FormsModule],
+  imports: [FooterComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
 })
 export class BlogsComponent implements OnInit {
+  formGroup: FormGroup = new FormGroup({});
   http = inject(HttpClient);
   blogs: any = [];
   title = 'Blogs';
@@ -21,22 +22,26 @@ export class BlogsComponent implements OnInit {
   readMoreIconUrl = 'assets/icons/arrowright.svg';
   filteredBlogs: any[] = []; // Filtered list of blogs
   searchQuery: string = ''; // Search query
+  search: any;
+
+  constructor(private router: Router, public blogsService: BlogsService, private apiService: ApiService, private fb: FormBuilder) {
+    this.formGroup = fb.group({ search: [''] });
+  }
   ngOnInit(): void {
     this.apiService.getAllBlogs().subscribe((data) => {
       this.blogs = Object.values(data);
       this.filteredBlogs = this.blogs;
     });
-  }
-  onFormSubmit(event: Event) {
-    event.preventDefault(); // Prevent default form submission behavior
-    this.filterBlogs(); // Call your filter function
+    this.formGroup.get('search')?.valueChanges.subscribe((value) => {
+      this.searchQuery = value;
+      this.filterBlogs(); // Call your filter function
+    });
   }
   filterBlogs() {
     console.log('filter is called');
     this.filteredBlogs = this.blogs.filter((blog: any) => blog.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
     console.log('🚀 ~ BlogsComponent ~ filterBlogs ~ ', this.searchQuery);
   }
-  constructor(private router: Router, public blogsService: BlogsService, private apiService: ApiService) {}
   goToBlogsDetails(blogId: number) {
     this.router.navigate(['/blogs/details'], { queryParams: { blogId: blogId } });
   }
