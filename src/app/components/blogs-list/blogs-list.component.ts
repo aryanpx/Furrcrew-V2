ec9261548958548da36caa256cca4538a4298d52
aryanpx <apitliya@bvurinnovations.com>
2024-05-11 12:31:33 +0530

    drag blogs done

--- src/app/components/blogs-list/blogs-list.component.ts
+++ src/app/components/blogs-list/blogs-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
@@ -9,15 +9,64 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './blogs-list.component.html',
   styleUrl: './blogs-list.component.css',
 })
-export class BlogsListComponent {
-  items: any[] = [];
-  goToBlogsDetails(blogId: number) {
-    this.router.navigate(['/blogs/details'], { queryParams: { blogId: blogId } });
-  }
-  constructor(private router: Router, private apiService: ApiService) {
+export class BlogsListComponent implements OnInit {
+  mouseDown = false;
+  startX!: number;
+  scrollLeft!: number;
+
+  constructor(private router: Router, private apiService: ApiService, private elementRef: ElementRef) {
     console.log('Blogs is running');
     this.apiService.getAllBlogs().subscribe((res) => {
       this.items = Object.values(res);
     });
+  }
+
+  ngOnInit(): void {}
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.move(event);
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.startDragging(event);
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    this.stopDragging(event);
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event: MouseEvent) {
+    this.stopDragging(event);
+  }
+
+  startDragging(event: MouseEvent) {
+    this.mouseDown = true;
+    const slider = this.elementRef.nativeElement.querySelector('.parent');
+    this.startX = event.pageX - slider.offsetLeft;
+    this.scrollLeft = slider.scrollLeft;
+  }
+
+  stopDragging(event: MouseEvent) {
+    this.mouseDown = false;
+  }
+
+  move(event: MouseEvent) {
+    event.preventDefault();
+    if (!this.mouseDown) {
+      return;
+    }
+    const slider = this.elementRef.nativeElement.querySelector('.parent');
+    const x = event.pageX - slider.offsetLeft;
+    const scroll = x - this.startX;
+    slider.scrollLeft = this.scrollLeft - scroll;
+  }
+
+  items: any[] = [];
+  goToBlogsDetails(blogId: number) {
+    this.router.navigate(['/blogs/details'], { queryParams: { blogId: blogId } });
   }
 }
