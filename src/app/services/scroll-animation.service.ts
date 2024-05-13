af45f96396d15bb0b86aa6b75efc4b4644decb38
aryanpx <apitliya@bvurinnovations.com>
2024-05-13 13:10:46 +0530

    scroll animation added side components and fixed lag

--- src/app/services/scroll-animation.service.ts
+++ src/app/services/scroll-animation.service.ts
@@ -17,20 +17,23 @@ export class ScrollAnimationService {
         var phoneHeight = $('.phone')?.height();
         if (phoneOffsetTop && scrollTop && windowHeight && phoneWidth && phoneHeight && windowWidth) {
           const scaleFactor = Math.max(0.67, Math.min(1, (windowHeight - scrollTop + phoneOffsetTop) / windowHeight));
+          const maxLeft = (windowWidth - phoneWidth) / 2;
+          const maxRight = maxLeft;
           if (windowWidth > 600) {
-            console.log('window greater than 600');
-            const rotationAngle = Math.max(0, Math.min((scrollTop - phoneOffsetTop) * 0.2, 90)) * -1;
+            const rotationAngle = Math.max(0, Math.min((scrollTop - phoneOffsetTop) * 0.1, 90)) * -1;
             const top = Math.abs(rotationAngle);
-            const left = Math.min(20, Math.abs(rotationAngle) / 5);
+            const left = Math.min(22, Math.abs(rotationAngle) / 2);
+            // const left = Math.min(maxLeft, Math.max(-maxRight, maxLeft - (maxLeft * scaleFactor) / 2));
+            const opacity = 1 - Math.max(0, Math.min((scrollTop - phoneOffsetTop) / windowHeight, 1)) * 6;
             if (rotationAngle > -90) {
-              const opacity = 1 - Math.max(0, Math.min((scrollTop - phoneOffsetTop) / windowHeight, 1)) * 6;
               $('.phone').css({
-                transform: `rotate(${rotationAngle}deg) scale(${scaleFactor})`,
-                position: Math.abs(rotationAngle) > 0 ? 'fixed' : 'static',
+                transform: `rotate(${rotationAngle}deg) scale(${scaleFactor}) `,
+                position: Math.abs(rotationAngle) > 0 ? 'fixed' : 'sticky',
                 left: `${left}%`,
                 top: `${top}px`,
               });
               $('.paragraph').css('opacity', opacity);
+              $('.phone_side_containers').css({ opacity: 0 });
             } else {
               $('.phone').css({
                 transform: `rotate(${rotationAngle}deg) scale(${scaleFactor})`,
@@ -38,24 +41,22 @@ export class ScrollAnimationService {
                 left: `${left}%`,
                 top: `${top}px`,
               });
-              console.log('scroll up');
+              $('.phone_side_containers').css({
+                opacity: 1,
+              });
             }
           } else if (windowWidth <= 600) {
-            console.log('window width is less than 600');
-            const phoneBottom = phoneOffsetTop + phoneHeight;
-            console.log('🚀 ~ ScrollAnimationService ~ $ ~ scrollTop:', scrollTop);
-            console.log('🚀 ~ ScrollAnimationService ~ $ ~ phoneBottom:', phoneBottom);
-            console.log('🚀 ~ ScrollAnimationService ~ $ ~ phoneOffsetTop:', phoneOffsetTop);
             const rotationAngle = Math.max(0, Math.min((scrollTop - phoneOffsetTop) * 0.2, 90)) * -1;
             const top = Math.abs(rotationAngle);
-            const left = Math.min(20, Math.abs(rotationAngle) / 5);
+            // const right = Math.min(maxRight, Math.abs(rotationAngle) / 5);
+            const right = Math.min(maxRight, Math.max(-maxLeft, maxRight - (maxRight * scaleFactor) / 2));
             if (rotationAngle > -90) {
-              console.log('🚀 ~ ScrollAnimationService ~ $ ~ rotationAngle:', rotationAngle);
               const opacity = 1 - Math.max(0, Math.min((scrollTop - phoneOffsetTop) / windowHeight, 1)) * 6;
               $('.phone').css({
                 transform: `rotate(${rotationAngle}deg) `,
                 position: Math.abs(rotationAngle) > 0 ? 'fixed' : 'static',
-                top: `${top * 2.5}px`,
+                top: `${top}px`,
+                right: `${right}px`,
               });
               $('.paragraph').css('opacity', opacity);
             } else {
