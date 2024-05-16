dd9827e8806e9f43980e4c3166ba50822e21dbe1
aryanpx <apitliya@bvurinnovations.com>
2024-05-16 10:25:03 +0530

    maps marker fix

--- src/app/components/events/upcoming-events/upcoming-events.component.ts
+++ src/app/components/events/upcoming-events/upcoming-events.component.ts
@@ -1,9 +1,10 @@
-import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FooterComponent } from '../../footer/footer.component';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { ApiService } from '../../../services/api.service';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { EventsService } from '../../../services/events.service';
 
 @Component({
   selector: 'app-upcoming-events',
@@ -20,16 +21,28 @@ export class UpcomingEventsComponent implements OnInit {
   events: any[] = [];
   event: any;
   isMobileView: boolean = false;
-
+  // markerOptions: google.maps.Marker = { draggable: false };
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
+  async initMap(): Promise<void> {
+    const { Map } = (await google.maps.importLibrary('maps')) as google.maps.MapsLibrary;
+    const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker')) as google.maps.MarkerLibrary;
+    const map = new Map(this.mapContainer.nativeElement, {
+      center: { lat: parseFloat(this.event.address.latitude), lng: parseFloat(this.event.address.longitude) },
+      zoom: 16,
+      mapId: '4504f8b37365c3d0',
+    });
+    new AdvancedMarkerElement({
+      map,
+      position: { lat: parseFloat(this.event.address.latitude), lng: parseFloat(this.event.address.longitude) },
+    });
+  }
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.checkScreenSize();
   }
-
   checkScreenSize() {
     this.isMobileView = window.innerWidth <= 768; // Adjust the breakpoint as needed
   }
-
   constructor(private route: ActivatedRoute, private apiService: ApiService, @Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
       this.checkScreenSize();
@@ -43,12 +56,12 @@ export class UpcomingEventsComponent implements OnInit {
   }
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      const eventId = params['eventId']; // Convert to number
+      const eventId = params['eventId'];
       this.getEvents(eventId);
     });
+    this.initMap();
   }
   isFlipped: boolean = false;
-
   flipCard() {
     if (!this.isMobileView) {
       this.isFlipped = !this.isFlipped;
@@ -56,7 +69,7 @@ export class UpcomingEventsComponent implements OnInit {
   }
   formatDate(unixTimestamp: number): string {
     const date = new Date(unixTimestamp * 1000);
-    return date.toUTCString(); // Adjust format as per your requirement
+    return date.toUTCString();
   }
   toggleModal() {
     this.showBookModal = !this.showBookModal;
